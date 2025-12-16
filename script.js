effaf5b8c51ba8ef128e608d0eb577347f15c64f
Lucas <lucas.benarble69@gmail.com>
2025-12-16 12:30:26 +0100

    Ajout bannerPage

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiUrl = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
+
+fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        displayBanner(data);
+
+
+    })
+    .catch(function (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+    });
+
+function displayBanner(data) {
+    const banner = document.getElementById("banner");
+
+
+}
